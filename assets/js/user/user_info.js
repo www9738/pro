6b415359398d3e616c6eb69cfbd41f5fd3edf347
geyunlong <973806612@qq.com>
2021-01-31 11:08:09 +0800

    完成user分支

---
+++ assets/js/user/user_info.js
@@ -0,0 +1,89 @@
+$(function () {
+    var form = layui.form
+    var layer = layui.layer
+
+    form.verify({
+        nickname: function (value) {
+            if (value.length > 6) {
+                return '昵称长度必须在 1 ~ 6 个字符之间！'
+            }
+        }
+    })
+
+
+    initUserInfo()
+    function initUserInfo() {
+        $.ajax({
+            type: "GET",
+            url: '/my/userinfo',
+
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('获取用户信息失败！')
+                }
+                form.val('formUserInfo', res.data)
+            }
+        });
+    }
+    $('#btnReset').click(function (e) {
+        e.preventDefault();
+        initUserInfo()
+    });
+    $('.layui-form').submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "/my/userinfo",
+
+            data: $(this).serialize(),
+
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('更新用户信息失败')
+
+                }
+                layer.msg('更新用户信息成功')
+                initUserInfo()
+                window.parent.getUserInfo()
+            }
+        });
+    });
+
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
